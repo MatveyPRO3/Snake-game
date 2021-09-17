29c295a715d840f9d9baead50de0f298c046eb73
Matvey <79414726+MatveyPRO3@users.noreply.github.com>
2021-09-17 10:20:37 +0300

    Bugs fixed and improvements

--- Snake_game/Snake_game.py
+++ Snake_game/Snake_game.py
@@ -8,10 +8,10 @@ pygame.mixer.init()
 width = 1200
 height = 800
 
-display  = pygame.display.set_mode((width,height))
+display = pygame.display.set_mode((width, height))
 
 pygame.display.update()
-pygame.display.set_caption("Snake_game")
+pygame.display.set_caption("Snake game")
 Icon = pygame.image.load("Icon_for_snake_game.ico")
 pygame.display.set_icon(Icon)
 
@@ -19,29 +19,33 @@ game_end = False
 
 clock = pygame.time.Clock()
 
+
 def clicksound():
     pygame.mixer.music.load("a.mp3")
     pygame.mixer.music.play(loops=0)
+
+
 colors = {
-    "snake_head": (0,255,0),
-    "snake_tail": (0,200,0),
-    "apple": (255,0,0)
+    "snake_head": (0, 255, 0),
+    "snake_tail": (0, 200, 0),
+    "apple": (255, 0, 0)
 }
 
+snake_size = (10, 10)
+
 snake_pos = {
-    "x": width/2-5,
-    "y": height/2-5,
+    "x": round(width//2 / 10)*10,
+    "y": round(height//2 / 10)*10,
     "x_change": 0,
     "y_change": 0
 }
 
-snake_size = (10,10)
 
 Pause = False
 
 food_pos = {
-    "x": round(random.randrange(0,width - snake_size[0]) / 10)*10,
-    "y": round(random.randrange(0,height - snake_size[1]) / 10)*10
+    "x": round(random.randrange(0, width - snake_size[0]) / 10)*10,
+    "y": round(random.randrange(0, height - snake_size[1]) / 10)*10
 
 }
 
@@ -50,22 +54,19 @@ snake_speed = 10
 snake_tails = []
 
 
+font = pygame.font.SysFont("arial", 20)
 
-font = pygame.font.SysFont("arial",20)
+font_for_pause = pygame.font.SysFont("arial", 20)
 
-font_for_pause = pygame.font.SysFont("arial",20)
-
-food_size = (10,10)
+food_size = (10, 10)
 food_eaten = 0
-score = font.render(str(food_eaten),1,(255,255,0),(150,150,150))
-
-
+score = font.render(str(food_eaten), 1, (255, 255, 0), (150, 150, 150))
 
 
 def check_KEYDOWN_events(event):
     if event.key == pygame.K_a and snake_pos["x_change"] == 0:
         snake_pos["x_change"] = -snake_speed
-        snake_pos["y_change"] = 0       
+        snake_pos["y_change"] = 0
     elif event.key == pygame.K_d and snake_pos["x_change"] == 0:
         snake_pos["x_change"] = snake_speed
         snake_pos["y_change"] = 0
@@ -74,8 +75,8 @@ def check_KEYDOWN_events(event):
         snake_pos["y_change"] = -snake_speed
     elif event.key == pygame.K_s and snake_pos["y_change"] == 0:
         snake_pos["x_change"] = 0
-        snake_pos["y_change"] = snake_speed 
-    
+        snake_pos["y_change"] = snake_speed
+
 
 Game_over = False
 
@@ -85,14 +86,13 @@ while not game_end:
             game_end = True
         elif event.type == pygame.KEYDOWN:
             check_KEYDOWN_events(event)
-       
 
-    display.fill((0,0,0))
+    display.fill((0, 0, 0))
 
     ltx = snake_pos["x"]
     lty = snake_pos["y"]
 
-    for i,v in enumerate(snake_tails):
+    for i, v in enumerate(snake_tails):
         _ltx = snake_tails[i][0]
         _lty = snake_tails[i][1]
 
@@ -102,14 +102,14 @@ while not game_end:
         ltx = _ltx
         lty = _lty
     for t in snake_tails:
-        pygame.draw.rect(display,colors["snake_tail"],[
+        pygame.draw.rect(display, colors["snake_tail"], [
             t[0],
             t[1],
             snake_size[0],
             snake_size[1]
-            ])    
+        ])
     snake_pos["x"] += snake_pos["x_change"]
-    snake_pos["y"] += snake_pos["y_change"] 
+    snake_pos["y"] += snake_pos["y_change"]
 
     if snake_pos["x"] < -snake_size[0]:
         snake_pos["x"] = width
@@ -125,9 +125,9 @@ while not game_end:
         snake_pos["y"],
         snake_size[0],
         snake_size[1]
-        ])
+    ])
 
-    pygame.draw.rect(display,colors["apple"],[
+    pygame.draw.rect(display, colors["apple"], [
         food_pos["x"],
         food_pos["y"],
         food_size[0],
@@ -135,29 +135,27 @@ while not game_end:
     ])
 
     if (snake_pos["x"] == food_pos["x"]
-        and snake_pos["y"] == food_pos["y"]):
-        food_eaten+=1
+            and snake_pos["y"] == food_pos["y"]):  # food eaten
+        food_eaten += 1
         clicksound()
-        snake_tails.append([food_pos["x"],food_pos["y"]])
+        snake_tails.append([food_pos["x"], food_pos["y"]])
         food_pos = {
-            "x": round(random.randrange(0,width-snake_size[0]) / 10)*10,
-            "y": round(random.randrange(0,height-snake_size[1]) / 10)*10
+            "x": round(random.randrange(0, width-snake_size[0]) / 10)*10,
+            "y": round(random.randrange(0, height-snake_size[1]) / 10)*10
         }
+        colors["apple"] = (random.randint(1,255),random.randint(1,255),random.randint(1,255))
 
-    for i,v in enumerate (snake_tails):
+    for i, v in enumerate(snake_tails):
         if(snake_pos["x"]+snake_pos["x_change"] == snake_tails[i][0]
-            and snake_pos["y"] + snake_pos["y_change"] == snake_tails[i][1]):
+                and snake_pos["y"] + snake_pos["y_change"] == snake_tails[i][1]):
             snake_tails = []
             food_eaten = 0
             break
-    if Pause:
-        Pause = font.render("PAUSE",1,(0,255,0),(0,0,0))
-        display.blit(score,(0,0))
     
-    score = font.render("Score: "+str(food_eaten),1,(255,255,0),(0,0,0))
-    display.blit(score,(50,50))
-    pygame.display.update()
 
+    score = font.render("Score: "+str(food_eaten), 1, (255, 255, 0), (0, 0, 0))
+    display.blit(score, (snake_pos["x"]+10, snake_pos["y"]-20))
+    pygame.display.update()
 
     clock.tick(30)
 
